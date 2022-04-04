a2d8911fef4a005166ee6b51fb04846a9747ece6
tufronicolas <ntufro@udesa.edu.ar>
2022-04-04 11:46:54 -0300

    controllers

--- proyectoIntegrador/app.js
+++ proyectoIntegrador/app.js
@@ -4,6 +4,11 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const indexRouter = require('./routes/index');
+const productsRouter = require('./routes/products');
+const usersRouter = require('./routes/users');
+
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -20,6 +25,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/', productsRouter);
+app.use('/', usersRouter);
+
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler

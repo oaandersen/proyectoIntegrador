f88704cf80198cbdc1b961ef8f67fc009d1a4766
tufronicolas <ntufro@udesa.edu.ar>
2022-04-07 11:14:40 -0300

    formularios

--- proyectoIntegrador/controllers/usersController.js
+++ proyectoIntegrador/controllers/usersController.js
@@ -1,8 +1,13 @@
 const usuario = require('../db/usuario');
+const router = require('../routes/users');
 
 const usersController = {
-    register: router.get('/', function(req, res) {
+  'register': router.get('/', function (req, res) {
     res.render('register');
   }),
+  'login': router.get('/', function (req, res) {
+    res.render('login')
+  }),
+
 }
-  module.exports = usersController;
+module.exports = usersController;
--- proyectoIntegrador/routes/users.js
+++ proyectoIntegrador/routes/users.js
@@ -1,9 +1,14 @@
 let express = require('express');
+const usersController = require('../controllers/usersController');
 let router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.get('/register', usersController.register)
+
+router.get('/login', usersController.register)
 
 module.exports = router;

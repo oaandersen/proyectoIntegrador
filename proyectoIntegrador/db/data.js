f3723a9f8ce9cb02be4d9815b4531c6da4e343c4
tufronicolas <ntufro@udesa.edu.ar>
2022-04-11 12:00:14 -0300

    data-products con detalles

--- proyectoIntegrador/db/data.js
+++ proyectoIntegrador/db/data.js
@@ -10,144 +10,126 @@ let data = {
 
     },
 
-    producto: {
-        lista: [{
-            imagen: '',
-            nombreProducto: '',
-            descripcion: '',
-            fechaCarga: '',
+    producto:  [{
+            imagen: '/images/products/img(1).jpg',
+            nombreProducto: 'Bugatti Veyron',
+            descripcion: 'El Bugatti Veyron es un automóvil superdeportivo biplaza.',
+            fechaCarga: '11/04/2022',
 
         }, {
 
-            imagen: '',
-            nombreProducto: '',
-            descripcion: '',
-            fechaCarga: '',
+            imagen: '/images/products/img(2).jpg',
+            nombreProducto: 'Audi R8',
+            descripcion: 'El Audi R8 es un automóvil deportivo de motor central.',
+            fechaCarga: '11/04/2022',
 
         }, {
 
-            imagen: '',
-            nombreProducto: '',
-            descripcion: '',
-            fechaCarga: '',
+            imagen: '/images/products/img(3).jpg',
+            nombreProducto: 'BMW I8',
+            descripcion: 'El BMW i8 es un automóvil híbrido eléctrico deportivo.',
+            fechaCarga: '11/04/2022',
 
         }, {
 
-            imagen: '',
-            nombreProducto: '',
-            descripcion: '',
-            fechaCarga: '',
+            imagen: '/images/products/img(4).jpg',
+            nombreProducto: 'Corvette Stingray',
+            descripcion: 'La versión Stingray posee un motor LT1 V8 atmosférico.',
+            fechaCarga: '11/04/2022',
 
         }, {
 
-            imagen: '',
-            nombreProducto: '',
-            descripcion: '',
-            fechaCarga: '',
+            imagen: '/images/products/img(5).jpg',
+            nombreProducto: 'Ford Mustang',
+            descripcion: 'El Ford Mustang es un automóvil deportivo muscle.',
+            fechaCarga: '11/04/2022',
 
         }, {
 
-            imagen: '',
-            nombreProducto: '',
-            descripcion: '',
-            fechaCarga: '',
+            imagen: '/images/products/img(11).jpg',
+            nombreProducto: 'Lamborghini Veneno',
+            descripcion: 'El Lamborghini Veneno es un automóvil superdeportivo.',
+            fechaCarga: '11/04/2022',
 
         }, {
 
-            imagen: '',
-            nombreProducto: '',
-            descripcion: '',
-            fechaCarga: '',
+            imagen: '/images/products/img(12).jpg',
+            nombreProducto: 'LaFerrari',
+            descripcion: 'El Ferrari LaFerrari es un automóvil superdeportivo híbrido.',
+            fechaCarga: '11/04/2022',
 
         }, {
 
-            imagen: '',
-            nombreProducto: '',
-            descripcion: '',
-            fechaCarga: '',
-
-        }, {
-
-            imagen: '',
-            nombreProducto: '',
-            descripcion: '',
-            fechaCarga: '',
-
-        }, {
-
-            imagen: '',
-            nombreProducto: '',
-            descripcion: '',
-            fechaCarga: '',
+            imagen: '/images/products/img(13).jpg',
+            nombreProducto: 'McLaren Senna',
+            descripcion: 'El auto lleva el nombre del piloto de carreras brasileño.',
+            fechaCarga: '11/04/2022',
 
         }]
-    },
-
-    comentarios: {
-        lista: [{
-            nombre: '',
-            comentario: '',
+    ,
+    comentarios: [{
+            nombre: 'Juan',
+            comentario: 'Una nave!',
             imagenPerfil: '',
 
         }, {
 
-            nombre: '',
-            comentario: '',
+            nombre: 'Ramiro',
+            comentario: 'El mejor lejos.',
             imagenPerfil: '',
 
         }, {
 
-            nombre: '',
-            comentario: '',
+            nombre: 'Esteban',
+            comentario: 'Espero algun dia poder probar uno.',
             imagenPerfil: '',
 
         }, {
 
-            nombre: '',
-            comentario: '',
+            nombre: 'Gonzalo',
+            comentario: 'Lo recomiendo, muy bueno.',
             imagenPerfil: '',
 
         }, {
 
-            nombre: '',
-            comentario: '',
+            nombre: 'Santiago',
+            comentario: 'Recomendable.',
             imagenPerfil: '',
 
         }, {
 
-            nombre: '',
-            comentario: '',
+            nombre: 'Hernan',
+            comentario: 'Una experiencia unica!',
             imagenPerfil: '',
 
         }, {
 
-            nombre: '',
-            comentario: '',
+            nombre: 'Fernando',
+            comentario: 'Mucho lujo.',
             imagenPerfil: '',
 
         }, {
 
-            nombre: '',
-            comentario: '',
+            nombre: 'Lucas',
+            comentario: 'Excelente diseño.',
             imagenPerfil: '',
 
         }, {
 
-            nombre: '',
-            comentario: '',
+            nombre: 'Tomas',
+            comentario: 'Grandes prestaciones.',
             imagenPerfil: '',
 
         }, {
 
-            nombre: '',
-            comentario: '',
+            nombre: 'Daniel',
+            comentario: 'Lindo color!',
             imagenPerfil: '',
 
         }, {
 
         }]
     }
-};
 
 
 

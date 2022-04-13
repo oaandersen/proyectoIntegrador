2ba81e686a9690ba9a3a86329df9c1deaae5d130
Oliver <oandersen@udesa.edu.ar>
2022-04-13 17:38:02 -0300

    detalles

--- proyectoIntegrador/controllers/indexController.js
+++ proyectoIntegrador/controllers/indexController.js
@@ -5,6 +5,8 @@ const indexController = {
 'index':function(req, res) { 
   res.render('index',{producto:data.producto});
   },
-
+'searchResults':function(req, res){
+  res.render('searchResults')
+}
 }
   module.exports = indexController;
--- proyectoIntegrador/routes/index.js
+++ proyectoIntegrador/routes/index.js
@@ -5,4 +5,6 @@ const indexController = require('../controllers/indexController');
 /* GET home page. */
 
 router.get('/', indexController.index)
+
+router.get('/searchResults', indexController.searchResults)
 module.exports = router;

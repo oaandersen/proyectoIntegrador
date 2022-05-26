6196cf4e6db0c6b2da63c7e9ed3f8d59edac358a
Oliver <oandersen@udesa.edu.ar>
2022-05-26 11:25:14 -0300

    modulos

--- proyectoIntegrador/app.js
+++ proyectoIntegrador/app.js
@@ -3,16 +3,47 @@ let express = require('express');
 let path = require('path');
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
+const session = require('express-session');
+const db = require('./db')
 
 const indexRouter = require('./routes/index');
 const productsRouter = require('./routes/product');
 const usersRouter = require('./routes/users');
+
 
 let app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+app.use(session({secret: "myApp",
+resave: false,
+saveUninitialized:true
+}));
+app.use(function(req,res,next){
+  if (req.session.user != undefined){
+    res.locals.user = req.session.user
+  }
+  return next();
+});
+
+app.use(function(req,res,next){
+  if (req.cookies.userId != undefined && req.session == undefined){
+let idUsuario = req.cookies.userId;
+
+db.User.findByPk(idUsuario)
+.then((user) => {
+req.sessinon.user = user.dataValues;
+res.locals.user = user.dataValues;
+return next();
+}).catch((err)=>{
+
+});
+  } else {
+
+  }
+})
 
 app.use(logger('dev'));
 app.use(express.json());

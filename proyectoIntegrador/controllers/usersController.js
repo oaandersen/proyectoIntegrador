b92e6185f0956e7a05c86ef768fd1b56a7beb5d4
tufronicolas <ntufro@udesa.edu.ar>
2022-06-14 17:03:28 -0300

    usuario

--- proyectoIntegrador/controllers/usersController.js
+++ proyectoIntegrador/controllers/usersController.js
@@ -22,52 +22,54 @@ const user = data.User;
 } */
 /* Requiriendo el modulo de bcryptjs .. */
 const bcrypt = require("bcryptjs");
+const db = require('../database/models');
 
 const usersController = {
-    login : (req, res) => {
-        return res.render("login");
-    },
-    procesarLogin : (req, res) => {
-       
-        let info = req.body;
+  login : (req, res) => {
 
-        user.findOne({
-            where : [{ email :  info.email}]
-        }).then((result) => {
-            if (result != null) {
+    return res.render("login");
+  },
+  procesarLogin : (req, res) => {
+    
+    let info = req.body;
+    
+    user.findOne({
+      where : [{ email :  info.email}]
+    }).then((result) => {
+      if (result != null) {
               let claveCorrecta = bcrypt.compareSync(info.password, result.password)
                 if (claveCorrecta == true && result.email == info.email) {
                   req.session.user = result.dataValues
+                  
+                  if (req.body.remember != undefined) {
+                    res.cookie('userId', req.session.user.id, { maxAge : 1000 * 60 * 5})
+                  } 
+                  return res.redirect('/')
+                  
+                }if (claveCorrecta == false){
+                  res.send("La contraseña ingresada es incorrecta!")
+                  
+                }}
+                if(result == null)
+                res.send(`El email ${info.email} no esta registrado!`)
                 
-                    if (req.body.remember != undefined) {
-                        res.cookie('userId', req.session.user.id, { maxAge : 1000 * 60 * 5})
-                      } 
-                      return res.redirect('/')
-
-                    }if (claveCorrecta == false){
-                      res.send("La contraseña ingresada es incorrecta!")
-                      
-                    }}
-                    if(result == null)
-                    res.send(`El email ${info.email} no esta registrado!`)
-                    
               });
-             // return res.redirect("/")
-
-        
-
-    },
-    register : (req, res) => {
-        return res.render("register");
-    },
+              // return res.redirect("/")
+              
+              
+              
+            },
+            register : (req, res) => {
+              return res.render("register");
+            },
     procesarRegister : (req, res) => {
         let info = req.body; //Captura todos los inputs guardados en el formulario
         let passEncriptada = bcrypt.hashSync(info.password, 10);
         let imgPerfil = req.file.filename;
         
-
+        
         let userParaGuardar = {
-         // created_at : new Date(),
+          // created_at : new Date(),
           //updated_at : new Date(),
           name : info.name,
           email : info.email,
@@ -78,9 +80,10 @@ const usersController = {
           
         }
         
+        
         user.create(userParaGuardar)
         .then((result) => {
-            return res.redirect("/users/login")
+          return res.redirect("/users/login")
         })
         
     }

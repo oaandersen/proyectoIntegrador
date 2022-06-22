b2927e24753a92033ddb4fb7ded047de02d5e9e3
AraujoJulian <araujoj@udesa.edu.ar>
2022-06-22 13:40:16 -0300

    models

--- proyectoIntegrador/database/models/Comment.js
+++ proyectoIntegrador/database/models/Comment.js
@@ -24,18 +24,18 @@ module.exports = function (sequelize, dataTypes) {
         underscored: true, 
     };
 
-    const comment = sequelize.define(alias, cols, config); // definiendo el modelo
-    comment.associate = function(modelos){
-        comment.belongsTo(modelos.producto,{
+    const Comment = sequelize.define(alias, cols, config); // definiendo el modelo
+    Comment.associate = function(modelos){
+        Comment.belongsTo(modelos.producto,{
             as: "commentsProduct",
             foreignKey: "producto_id"
         }); 
     }
-    comment.associate = function(modelos){
-        comment.belongsTo(modelos.User,{
+    Comment.associate = function(modelos){
+        Comment.belongsTo(modelos.User,{
             as: "commentsUser",
             foreignKey: "cm_user_id"
         }); 
     }
-    return comment;
+    return Comment;
 }
